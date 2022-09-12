85598a6018a078286e4f8382d91b91a2f4ad4a87
Ola Wypijewska <Ola.Wypijewska@gmail.com>
2022-09-12 20:42:16 +0100

    MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String name = "The Earth";
+        double orbitalSpeed =29.78;
+        int satellites =1;
+        double surfaceGravity =9.8;
+        String shape = "oblate spheroid";
+        String galaxy ="Milky Way";
+        System.out.println(name);
+        System.out.println(orbitalSpeed + " km/s");
+        System.out.println(satellites);
+        System.out.println(surfaceGravity + " m/s2");
+        System.out.println(shape);
+        System.out.println(galaxy);
+    }
+    }
