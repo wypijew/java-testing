d47513768d69fa0301df328f22adf6e778b87792
Ola Wypijewska <Ola.Wypijewska@gmail.com>
2022-09-12 19:53:23 +0100

    kodilla-intro

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+public static void main(String[] args) {
+    System.out.println("Hello world!");
+    String name ="Ola";
+    int age =36;
+    char first_letter ='W';
+System.out.println(name + "\n" + age + "\n" + first_letter + "\n");}
+}
