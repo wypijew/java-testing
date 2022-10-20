119a6c2957222638686766c2e113cc1680ceb0f2
Ola Wypijewska <Ola.Wypijewska@gmail.com>
2022-10-20 03:54:44 +0100

    20.10.2022

---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,22 @@
+
+public class Grades {
+
+    private int[] grades;
+    private int size;
+
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+
+}
+
---
+++ kodilla-intro/src/main/java/User.java
@@ -0,0 +1,26 @@
+public class User {
+    public static void main(String[] args) {
+
+//        String[] users = new String[6];
+//        users[0] = "Klapouchy";
+//        users[1] = "Prosiaczek";
+//        users[2] = "Krzys";
+//        users[3] = "Tygrysek";
+//        users[4] = "Sowa";
+//        users[5] = "Malenstwo";
+//
+//        for (int i = 0; i < users.length; i++) {
+//            System.out.println(users[i]);
+
+//        User klapouchy = new User("Klapouchy", 15);
+//        User prosiaczek = new User("Prosiaczek", 10);
+//        User krzys = new User("Krzys", 8);
+//        User tygrysek = new User("Tygrysek", 12);
+//        User sowa = new User("Sowa", 20);
+//        User malenstwo = new user("Malenstwo", 5);
+//
+//        User[] users = {klapouchy, prosiaczek, krzys, tygrysek, sowa, malenstwo};
+
+
+        }
+    }
