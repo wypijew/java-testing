f4be469bc8ad53893099bde95612ed7b3df485fb
Ola Wypijewska <Ola.Wypijewska@gmail.com>
2023-02-08 06:53:29 +0000

    7022023

---
+++ kodilla-basic-tests/src/test/java/com/kodilla/ResultChecker1.java
@@ -0,0 +1,8 @@
+package com.kodilla;
+
+public class ResultChecker1 {
+
+    public static boolean assertEquals1(double expected, double actual, double delta) {
+        return expected == actual;
+    }
+}
