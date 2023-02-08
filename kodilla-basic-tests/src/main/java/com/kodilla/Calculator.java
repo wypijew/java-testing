8998e0c6cbf4ef977d6600c94bffed46c406ff13
Ola Wypijewska <Ola.Wypijewska@gmail.com>
2023-02-08 06:50:09 +0000

    7/02/2023

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,19 @@
+package com.kodilla;
+
+public class Calculator {
+
+    public int sum(int a, int b) {
+
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+
+        return a - b;
+    }
+
+    public double square(double a) {
+
+        return a * a;
+    }
+}
---
+++ kodilla-basic-tests/src/main/java/com/kodilla/ResultChecker.java
@@ -0,0 +1,10 @@
+package com.kodilla;
+
+public class ResultChecker {
+
+    public static boolean assertEquals(int expected, double actual) {
+
+        return expected == actual;
+    }
+
+}
